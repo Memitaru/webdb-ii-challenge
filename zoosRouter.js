431d4f1e74ccd05f56513ed62ec413add8382c82
Memitaru <amandatwah@gmail.com>
2019-05-26 20:23:42 -0700

    GET and GET by id requests work, zoo endpoints set up on a router

---
+++ zoosRouter.js
@@ -0,0 +1,41 @@
+const knex = require('knex');
+const router = require('express').Router();
+
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+        filename: './data/lambda.sqlite3'
+    },
+    useNullAsDefault: true
+}
+
+const db = knex(knexConfig);
+
+router.get('/', (req, res) => {
+    db('zoos')
+        .first()
+        .then(zoos => {
+            res.status(200).json(zoos)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+router.get('/:id', (req, res) => {
+    db('zoos')
+        .where({id: req.params.id})
+        .first()
+        .then(zoo => {
+            if (zoo) {
+                res.status(200).json(zoo)
+            } else {
+                res.status(404).json({message: "No zoo found"})
+            }
+        })
+        .catch(err =>{
+            res.status(500).json(err)
+        })
+})
+
+module.exports = router;
