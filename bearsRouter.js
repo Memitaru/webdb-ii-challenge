3308fd726539c8d5cdce3f004f08a532700ffe4d
Memitaru <amandatwah@gmail.com>
2019-05-27 20:08:45 -0700

    All stretch done

--- bearsRouter.js
+++ bearsRouter.js
@@ -22,4 +22,63 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    db('bears')
+        .where({id: req.params.id})
+        .first()
+        .then(bear => {
+            if (bear){
+                res.status(200).json(bear)
+            } else {
+                res.status(404).json({message: "No bear at this ID"})
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+router.post('/', (req, res) => {
+    db('bears')
+        .insert(req.body, 'id')
+        .then(results => {
+            res.status(200).json(results[0])
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+router.put('/:id', (req, res) => {
+    db('bears')
+        .where({id: req.params.id})
+        .update(req.body)
+        .then(count => {
+            if (count) {
+                res.status(200).json(count)
+            } else {
+                res.status(404).json({message: "No bear at that ID"})
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    db('bears')
+        .where({id: req.params.id})
+        .del()
+        .then(count => {
+            if (count) {
+                res.status(200).json(count)
+            } else {
+                res.status(404).json({message: "No bear at that ID"})
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
 module.exports = router;
